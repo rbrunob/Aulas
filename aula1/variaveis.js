b8906bd5883a641064540571419f4a2edfb50e6a
bruno.ramos <bruno.bragaramos@gmail.com>
2022-03-29 14:01:20 -0300

    first commit

---
+++ aula1/variaveis.js
@@ -0,0 +1,13 @@
+//string
+var texto = 'Curso de JavaScript';
+
+//number
+var numeroInteiro = -7; //usasse na documentação termos como: "INT" 
+var numeroFracionado = 11210.75; // usasse na documentação termos como: "FLOAT, DUBLE, PONTO FLUTUANTE"
+
+//boolean
+var teste = true; // true -> verdadeiro / false -> falso
+
+// alert(numeroFracionado) //abrir um dialog
+// document.write(texto) //constroi um novo nó no DOM
+// console.log(numeroInteiro) //exibe no console
---
+++ aula2/contatenacao.js
@@ -0,0 +1,4 @@
+var nome = prompt('Digite o seu nome');
+var idade = prompt('Digite a sua idade:');
+
+document.write('<h1>Olá ' + nome + ' , tudo bem? Você tem ' + idade + ' anos?</h1>')
